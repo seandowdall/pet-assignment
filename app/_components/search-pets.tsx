aadceb7fd612d3165e1f0e79d4991432bca428d7
Sean Dowdall <seanied1234567@gmail.com>
2024-02-28 13:44:16 +0000

    ui improvments

--- app/_components/search-pets.tsx
+++ app/_components/search-pets.tsx
@@ -1,7 +1,10 @@
 "use client";
 
-import { Pet } from "@/types/type";
 import React, { useState } from "react";
+import PetCard from "./pet-card"; // Import PetCard component
+import { Pet } from "@/types/type";
+import { Button } from "@/components/ui/button";
+import { Search } from "lucide-react";
 
 const SearchPets = () => {
   const [type, setType] = useState("Dog");
@@ -22,38 +25,40 @@ const SearchPets = () => {
       const data = await response.json();
       setResults(data);
     } catch (e) {
-      setError("Failed to fetch available dogs.");
+      setError("Failed to fetch available pets.");
       console.error("There was an error!", e);
     }
   };
 
   return (
     <div>
-      <label>
-        Type:
-        <select value={type} onChange={(e) => setType(e.target.value)}>
-          <option value="Dog">Dog</option>
-          <option value="Cat">Cat</option>
-          {/* ... other types ... */}
-        </select>
-      </label>
-      <label>
-        Status:
-        <select value={status} onChange={(e) => setStatus(e.target.value)}>
-          <option value="Available">Available</option>
-          {/* ... other statuses ... */}
-        </select>
-      </label>
-      <button onClick={handleSearch}>Search</button>
+      <div className=" flex flex-row space-x-5 items-center">
+        <label>
+          Type:
+          <select value={type} onChange={(e) => setType(e.target.value)}>
+            <option value="Dog">Dog</option>
+            <option value="Cat">Cat</option>
+            {/* ... other types ... */}
+          </select>
+        </label>
+        <label>
+          Status:
+          <select value={status} onChange={(e) => setStatus(e.target.value)}>
+            <option value="Available">Available</option>
+            <option value="Unavailable">Unavailable</option>
+            {/* ... other statuses ... */}
+          </select>
+        </label>
+        <Button onClick={handleSearch}>
+          <Search />
+        </Button>
+      </div>
+
       {error && <p>{error}</p>}
-      <div>
-        {/* Render your results here */}
-        {results.map((pet, index) => (
-          <div key={index}>
-            {/* Display pet details */}
-            <p>{pet.name}</p>
-            {/* ... */}
-          </div>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-5">
+        {/* This will render the pet cards in a 3-column grid on medium screens and above */}
+        {results.map((pet) => (
+          <PetCard key={pet.id} pet={pet} />
         ))}
       </div>
     </div>
